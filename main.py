2c90e18968379d3731d45b01bdb0931b46ee6cf8
davidtc8 <davidtorresc8@gmail.com>
2021-01-10 16:56:01 -0600

    Code that create all the letters

--- main.py
+++ main.py
@@ -6,3 +6,39 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+PLACEHOLDER = "[name]"
+# 2nd test
+import docx
+import os
+
+
+
+file_path = r"C:\Users\David Torres\Desktop\Programming\Python\100 Days of Code Course\Birtthday_invitation_letters_automation\Input\Names\invited_names.txt"
+os.path.basename(file_path)
+with open(file_path, mode = "r") as file:
+    names = file.readlines()
+    print(names)
+
+# function to read the docx file
+def reading_text(filename):
+    doc = docx.Document(filename)
+
+    completed_text = []
+
+    for paragraph in doc.paragraphs:
+        completed_text.append(paragraph.text)
+
+    return '\n'.join(completed_text)
+
+print(reading_text('starting_letter.docx'))
+
+letter_content = reading_text('starting_letter.docx')
+
+for name in names:
+    stripped_name = name.strip()
+    new_letter = letter_content.replace(PLACEHOLDER, stripped_name)
+    print(new_letter)
+    with open(f'/Users/David Torres/Desktop/Programming/Python/100 Days of Code Course/Birtthday_invitation_letters_automation/Output/ReadyToSend/letter_for_{stripped_name}.docx', mode = 'w') as completed_letter:
+        completed_letter.write(new_letter)
